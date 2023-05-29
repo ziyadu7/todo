cf96f56548da5573214a1699b4bde0bc3a99b405
Muhammed Ziyad <muhdziyad@gmail.com>
2023-05-29 16:09:59 +0530

    toDo first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState,useRef} from 'react'
 function App() {
+  const [toDo,setToDo] = useState('')
+  const [toDos,setToDos] = useState([])
+  const inputRef = useRef(null)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <div className = 'mainHeading'>
+      <h1>ToDo List</h1>
+    </div>
+    <div className = 'subHeading'>
+      <h2>Yaaayy!!, it's Sunday...</h2>
+    </div>
+    <div className='input'>
+      <input  ref = {inputRef} value={toDo} onChange={(e)=>{setToDo(e.target.value)}} type='text' placeholder='Add lists..'></input>
+      {/* <button className='addButton'>Add</button> */}
+      <i onClick={()=>{
+        if(toDo.trim()!==''){
+          setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])
+          inputRef.current.focus()
+        }else{
+          alert('Please fill the field')
+        }
+    }} className='fas fa-plus'></i>
+    </div>
+    <div className='todos'>
+          {toDos.map((toDo,i)=>{
+           return( <div className='todo'>
+           <div className='left'>
+           <input type='checkbox' onClick={(e)=>{setToDos( toDos.filter((obj)=>{
+            if(obj.id===toDo.id){
+              obj.status = e.target.checked
+            }
+            return obj
+           }))}} name='' id=''></input>
+           <p>{toDo.text}</p>
+         </div>
+         <div className='right'>
+         <i onClick={()=>{setToDos(toDos.filter((obj)=>{
+                    if(obj.id===toDo.id){
+                      obj = null
+                    }
+                    return obj
+         }))}} className='fas fa-remove'></i>
+         </div>
+       </div>) 
+          })}
+    </div>
     </div>
   );
 }
